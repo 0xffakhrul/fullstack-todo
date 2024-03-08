a52bc3a71828029fc3f0d6100e98f06b95fc376c
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-03-08 22:00:11 +0800

    add todo count

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,29 +1,31 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
+import { useFetchTodos } from "../hooks/useTodo";
 import { useQuery } from "@tanstack/react-query";
 
 interface HeaderProps {
-  // tasks: Task[];
+  
 }
 
 const Header: FC<HeaderProps> = ({}) => {
-  // const {
-  //   isLoading,
-  //   data: todos,
-  //   error,
-  // } = useQuery({
-  //   queryKey: ["todos"],
-  //   queryFn: todoCount,
-  // });
+  const {
+    isLoading,
+    data: todos,
+    error,
+  } = useQuery({ queryKey: ["todos"], queryFn: useFetchTodos });
 
-  // console.log(todos);
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
 
-  // if (isLoading) return "Loading...";
-  // if (error) return "An error has occurred: " + error.message;
+  console.log(todos);
+
+  const openTasks = todos.filter((todo: any) => !todo.is_completed);
+
+  console.log(openTasks);
 
   return (
     <div className="pb-4">
       <h1 className="font-bold text-3xl pb-4 text-secondary">
-        You have sdsfd
+        You have {openTasks.length} open tasks.
       </h1>
     </div>
   );
